42ee224e385b67de276bfeecd33abf2ce28aef72
Anton_Tsyhankou <Anton_Tsyhankou@epam.com>
2021-01-06 17:04:53 +0300

    1

--- converter/utils.py
+++ converter/utils.py
@@ -2,19 +2,14 @@ import io
 import os
 import urllib.parse
 from werkzeug.utils import secure_filename
-from converter.converter_2_pdf import generate_new_name, process_content,\
-     convert_to_user_format
+from converter.converter_2_pdf import generate_new_name, process_content
 
 
 def allowed_file(some_obj) -> bool:
-    """Return True if extension is allowed or if link on repo"""
     allowed_extens = ['md', 'rtf']
     if some_obj.startswith('http://') or some_obj.startswith('https://'):
         url_schema = urllib.parse.urlparse(some_obj)
-        if '.' in url_schema.path.split('/')[-1]:
-            return url_schema.path.split('.')[-1] in allowed_extens
-        else:
-            return True
+        return url_schema.path.split('.')[-1] in allowed_extens
     else:
         return '.' in some_obj and some_obj.split('.')[-1] in allowed_extens
 
@@ -25,9 +20,8 @@ def get_content(flask_request):
     file_name = url or secure_filename(file.filename)
     output_format = flask_request.form.get('formatList')
     if allowed_file(file_name):
-        new_filename = generate_new_name(file_name, output_format)
-        convert_to_user_format(process_content(file, url), new_filename)
-        return new_filename
+        return process_content(file, url), generate_new_name(
+            file_name, output_format)
 
 
 def copy_file_and_remove(filename: str):
