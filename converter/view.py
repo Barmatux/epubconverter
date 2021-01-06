b42d3491ac2763a7aec2a23dbd161b5666c3089a
Anton_Tsyhankou <Anton_Tsyhankou@epam.com>
2021-01-06 17:03:52 +0300

    1

--- converter/view.py
+++ converter/view.py
@@ -8,11 +8,10 @@ from converter.utils import get_content, copy_file_and_remove
 def upload_page():
     if request.method == 'POST':
         try:
-            new_file_name = get_content(request)
-            if not new_file_name:
-                raise AttributeError
-        except AttributeError:
+            some_obj, new_file_name = get_content(request)
+        except TypeError:
             return redirect(url_for('exception_page'))
+        convert_to_user_format(some_obj, new_file_name)
         return redirect(url_for('uploaded_file', filename=new_file_name))
     return render_template('index.html')
 
@@ -22,14 +21,6 @@ def uploaded_file(filename):
     mimetype, encoding = mimetypes.guess_type(filename)
     return_data = copy_file_and_remove(filename)
     return send_file(return_data, mimetype=mimetype)
-
-
-@flask_app.route('/convert-repo', methods=['GET', 'POST'])
-def converted_page():
-    if request.method == 'POST':
-        filename = get_content(request)
-        return redirect(url_for('uploaded_file', filename=filename))
-    return render_template('convert_repo.html')
 
 
 @flask_app.route('/exception')
